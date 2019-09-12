85e34da1aad1c9145e208def4c982a65c0a87559
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-09-12 17:30:46 +0200

    some stuffs

--- _commands/_simple-commands.bash
+++ _commands/_simple-commands.bash
@@ -1,3 +1,10 @@
 # replace the icon config
 sudo cp _httpd/conf.d-icons.conf /etc/httpd/conf.d/icons.conf && sudo systemctl restart httpd; systemctl status httpd
 
+# replace with documentation
+sudo cp _httpd/conf.d-documentation.conf /etc/httpd/conf.d/docs.conf && sudo systemctl restart httpd; systemctl status httpd
+
+# trying a topher
+sudo cp web/_httpd/conf.d-docs-psql.conf /etc/httpd/conf.d/docs-psql.conf && \
+    sudo cp web/_httpd/conf.d-docs-icons.conf /etc/httpd/conf.d/docs-icons.conf && \
+    sudo systemctl restart httpd; systemctl status httpd
