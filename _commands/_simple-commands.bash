848e2f00649a5617f130474de56a219f27c42047
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-09-17 22:09:54 +0200

    stuffs

--- _commands/_simple-commands.bash
+++ _commands/_simple-commands.bash
@@ -8,3 +8,17 @@ sudo cp _httpd/conf.d-documentation.conf /etc/httpd/conf.d/docs.conf && sudo sys
 sudo cp web/_httpd/conf.d-docs-psql.conf /etc/httpd/conf.d/docs-psql.conf && \
     sudo cp web/_httpd/conf.d-docs-icons.conf /etc/httpd/conf.d/docs-icons.conf && \
     sudo systemctl restart httpd; systemctl status httpd
+
+# removeing the icons virthost, and u´only using the psql docs one
+# it seems they cannot coexist, they dont know of each other?
+sudo rm /etc/httpd/conf.d/docs-icons.conf && \
+    sudo systemctl restart httpd; systemctl status httpd
+
+# includes example
+sudo cp web/_documentation/includes.html web/_documentation/footer.html /var/www/docs/includes/ && \
+    sudo cp web/_httpd/includes.example.conf /etc/httpd/conf.d/includes.example.conf && \
+    sudo systemctl restart httpd; systemctl status httpd
+
+# unified docs.conf
+sudo cp web/_httpd/conf.d-docs.conf /etc/httpd/conf.d/docs.conf && \
+    sudo systemctl restart httpd; systemctl status httpd
